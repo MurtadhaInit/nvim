6ac14684b4a90d1e8182bc855a27b55c4192dd27
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-10-25 15:52:26 +0100

    Added Catppuccin settings (transparent background)

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,8 +4,8 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- after selecting text you can move it up or down with Shift + k / j
- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+ -- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+ -- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Resize with arrows
 -- keymap("n", "<C-Up>", ":resize +2<CR>", opts)
--- lua/core/plugin_config/catppuccin.lua
+++ lua/core/plugin_config/catppuccin.lua
@@ -1,2 +1,23 @@
--- Choose from: catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-vim.cmd.colorscheme "catppuccin-mocha"
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = true,
+    dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    integrations = {
+    --     cmp = true,
+    --     gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+    --     notify = false,
+    --     mini = {
+    --         enabled = true,
+    --         indentscope_color = "",
+    --     },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+vim.cmd.colorscheme "catppuccin"
