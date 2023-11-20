cdb1976490c6aa9522451a82f99836c563da3567
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-11-20 02:06:12 +0000

    Remove Catppuccin setting to dim inactive windows

--- lua/core/plugin_config/catppuccin.lua
+++ lua/core/plugin_config/catppuccin.lua
@@ -1,11 +1,6 @@
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     transparent_background = true,
-    dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
     integrations = {
     --     cmp = true,
     --     gitsigns = true,
